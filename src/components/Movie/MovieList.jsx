e3b682abb045ad6c5d649a10446805487f854193
Joao Vasco <joaovascosemblanodev@gmail.com>
2022-12-03 03:38:35 +0000

    added pagination

--- src/components/Movie/MovieList.jsx
+++ src/components/Movie/MovieList.jsx
@@ -1,18 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MovieItem from "./MovieItem";
 import { getMovies } from "../../services/api";
 import movieStyles from "./Movie.module.css";
 import Loading from "../Loading/Loading";
 
 const MovieList = ({ query }) => {
+  const MIN_PAGE = 1;
   const [movieList, setMovieList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(MIN_PAGE);
+  const [maxPage, setMaxPage] = useState(MIN_PAGE);
+  let lastQuery = useRef(query);
 
   useEffect(() => {
+    let controller = new AbortController();
+    let signal = controller.signal;
+
     const fetchData = () => {
+      if (lastQuery.current !== query) {
+        setPage(() => MIN_PAGE);
+      }
       setLoading(() => true);
-      getMovies(query)
-        .then((data) => setMovieList(() => data.results))
+      getMovies(query, page, signal)
+        .then((data) => {
+          setMovieList(() => data.results);
+          setMaxPage(() => data.total_pages);
+          lastQuery.current = query;
+        })
         .catch((error) => console.log(error))
         .finally(() => setLoading(() => false));
     };
@@ -23,19 +37,57 @@ const MovieList = ({ query }) => {
       } else {
         setMovieList(() => []);
       }
-    }, 500);
+    }, 100);
 
-    return () => clearTimeout(delay);
-  }, [query]);
+    return () => {
+      clearTimeout(delay);
+      controller.abort();
+    };
+  }, [query, page]);
+
+  function handlePageChange(previousOrNext) {
+    switch (previousOrNext) {
+      case "previous":
+        setPage((prevPage) => prevPage - 1);
+        break;
+      case "next":
+        setPage((prevPage) => prevPage + 1);
+        break;
+      default:
+        break;
+    }
+  }
 
   return loading ? (
     <Loading />
   ) : (
-    <div className={movieStyles.movieGrid}>
-      {movieList.map((movie, index) => (
-        <MovieItem key={`movie${index}`} movie={movie} />
-      ))}
-    </div>
+    <>
+      {!!movieList.length && (
+        <div className={movieStyles.pageSelectors}>
+          {page > MIN_PAGE && (
+            <div
+              className={movieStyles.pageArrow}
+              onClick={() => handlePageChange("previous")}
+            >
+              ← Previous Page
+            </div>
+          )}
+          {page < maxPage && (
+            <div
+              className={movieStyles.pageArrow}
+              onClick={() => handlePageChange("next")}
+            >
+              Next Page →
+            </div>
+          )}
+        </div>
+      )}
+      <div className={movieStyles.movieGrid}>
+        {movieList.map((movie, index) => (
+          <MovieItem key={`movie${index}`} movie={movie} />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 const API_BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY_QUERY = `api_key=${process.env.REACT_APP_API_KEY}`;
 
-export async function getMovies(searchQuery) {
+export async function getMovies(searchQuery, page, signal) {
   const response = await fetch(
     `${API_BASE_URL}/search/movie?${API_KEY_QUERY}${
       searchQuery ? `&query=${encodeURI(searchQuery)}` : ""
-    }`
+    }&page=${page}`,
+    { signal }
   );
   const data = await response.json();
   return data;
